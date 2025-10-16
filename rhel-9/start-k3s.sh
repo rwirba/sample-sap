47caf84efb16a2a95d73616c38cbd6f8162d66a1
rwirba <rwirba@protonmail.com>
2025-10-15 21:34:12 -0500

    fixes

--- rhel-9/start-k3s.sh
+++ rhel-9/start-k3s.sh
@@ -4,14 +4,5 @@ set -e
 export CONTAINERD_SNAPSHOTTER=fuse-overlayfs
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-echo "Starting K3s..."
-k3s server --disable traefik --tls-san 127.0.0.1 &
-
-echo "Waiting for Kubernetes API..."
-until kubectl get nodes &>/dev/null; do sleep 2; done
-
-kubectl get nodes
-kubectl get pods -A
-
-# Keep container alive
-tail -f /dev/null
+echo "Starting K3s in foreground..."
+exec k3s server --disable traefik --tls-san 127.0.0.1
