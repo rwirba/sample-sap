d39bfd2c6be535a26d5e7bd2deeadbe90cb969d7
rwirba <rwirba@protonmail.com>
2025-10-15 21:39:52 -0500

    updates

--- rhel-9/start-k3s.sh
+++ rhel-9/start-k3s.sh
@@ -4,5 +4,8 @@ set -e
 export CONTAINERD_SNAPSHOTTER=fuse-overlayfs
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-echo "Starting K3s in foreground..."
-exec k3s server --disable traefik --tls-san 127.0.0.1
+echo "Starting K3s with user namespace support..."
+exec k3s server \
+  --disable traefik \
+  --tls-san 127.0.0.1 \
+  --kubelet-arg="feature-gates=KubeletInUserNamespace=true"
