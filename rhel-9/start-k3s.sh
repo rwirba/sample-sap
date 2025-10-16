cc138a17814c12e5b75dbbf3b8f0384a95c82518
rwirba <rwirba@protonmail.com>
2025-10-16 03:34:06 -0500

    updates

--- rhel-9/start-k3s.sh
+++ rhel-9/start-k3s.sh
@@ -4,11 +4,15 @@ set -e
 export CONTAINERD_SNAPSHOTTER=fuse-overlayfs
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-echo "Starting K3s with container-safe kubelet args..."
+echo "Starting K3s with container-safe runtime and cgroup settings..."
 exec k3s server \
   --disable traefik \
   --tls-san 127.0.0.1 \
   --kubelet-arg="feature-gates=KubeletInUserNamespace=true" \
   --kubelet-arg="cgroups-per-qos=false" \
   --kubelet-arg="cgroup-root=/" \
-  --kubelet-arg="enforce-node-allocatable="
+  --kubelet-arg="enforce-node-allocatable=" \
+  --kubelet-arg="container-runtime-endpoint=unix:///run/k3s/containerd/containerd.sock" \
+  --kubelet-arg="runtime-cgroups=" \
+  --kubelet-arg="kubelet-cgroups=" \
+  --kubelet-arg="systemd-cgroup=false"
