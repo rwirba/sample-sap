ece6f95cd71adf8ddfbbaa3d99d74faec3eb560e
rwirba <rwirba@protonmail.com>
2025-10-15 21:42:17 -0500

    updates

--- rhel-9/start-k3s.sh
+++ rhel-9/start-k3s.sh
@@ -4,8 +4,10 @@ set -e
 export CONTAINERD_SNAPSHOTTER=fuse-overlayfs
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-echo "Starting K3s with user namespace support..."
+echo "Starting K3s with container-safe kubelet args..."
 exec k3s server \
   --disable traefik \
   --tls-san 127.0.0.1 \
-  --kubelet-arg="feature-gates=KubeletInUserNamespace=true"
+  --kubelet-arg="feature-gates=KubeletInUserNamespace=true" \
+  --kubelet-arg="cgroups-per-qos=false" \
+  --kubelet-arg="cgroup-root=/"
