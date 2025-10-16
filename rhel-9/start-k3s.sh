2646708fb4002a33e9a8be5c41dd525208a15f3c
rwirba <rwirba@protonmail.com>
2025-10-15 21:24:45 -0500

    updates

--- rhel-9/start-k3s.sh
+++ rhel-9/start-k3s.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "Starting K3s with fuse-overlayfs..."
 export CONTAINERD_SNAPSHOTTER=fuse-overlayfs
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
+echo "Starting K3s..."
 k3s server --disable traefik --tls-san 127.0.0.1 &
 
-echo "Waiting for Kubernetes API to be ready..."
-until kubectl get nodes &>/dev/null; do
-  sleep 2
-done
+echo "Waiting for Kubernetes API..."
+until kubectl get nodes &>/dev/null; do sleep 2; done
 
 kubectl get nodes
 kubectl get pods -A
 
-exec /bin/bash
+# Keep container alive
+tail -f /dev/null
