070e29b0893faa05574ce855740a91647cf4b259
rwirba <rwirba@protonmail.com>
2025-10-21 21:57:07 -0500

    updates

--- minikube/java-hello-world/redeploy.sh
+++ minikube/java-hello-world/redeploy.sh
@@ -39,16 +39,13 @@ docker push "$IMAGE_FULL"
 
 echo "📦 Packaging Helm chart..."
 rm -f ${CHART_DIR}/${APP_NAME}-*.tgz || true
-helm package "${CHART_DIR}" -d "${CHART_DIR}"
-
-# Automatically detect the packaged chart file
-CHART_PKG=$(ls ${CHART_DIR}/${APP_NAME}-*.tgz | head -n 1)
+helm lint "${CHART_DIR}" >/dev/null || true  # optional lint step
 
 MINIKUBE_IP=$(minikube ip)
 INGRESS_HOST="${MINIKUBE_IP}.nip.io"
 
 echo "🚀 Redeploying Helm release..."
-helm upgrade --install "$RELEASE_NAME" "${CHART_PKG}" \
+helm upgrade --install "$RELEASE_NAME" "$CHART_DIR" \
   --namespace "$NAMESPACE" \
   --create-namespace \
   --set image.repository="docker.io/${DOCKER_USER}/${APP_NAME}" \
