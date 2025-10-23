6d990dc2d7accd4302c921b92fbec95d657b8cd9
rwirba <rwirba@protonmail.com>
2025-10-22 22:37:44 -0500

    updated

--- minikube/java-hello-world/update.sh
+++ minikube/java-hello-world/update.sh
@@ -7,12 +7,5 @@ echo "🔁 Updating app in Minikube..."
 helm upgrade hello-java . \
   --namespace demo 
 
-# Ensure minikube tunnel is running
-if ! pgrep -f "minikube tunnel" > /dev/null; then
-  echo "🔌 Starting minikube tunnel in background..."
-  nohup sudo minikube tunnel > /dev/null 2>&1 &
-else
-  echo "✅ Minikube tunnel is already running."
-fi
-
-echo "✅ Update complete. App refreshed at: http://$(curl -s http://checkip.amazonaws.com).nip.io/"
+pkill -f "minikube tunnel"
+minikube tunnel --alsologtostderr -v=8
