c04dcc63131d7d89856cb8eb80e0f608dab5291c
rwirba <rwirba@protonmail.com>
2025-10-23 11:48:52 -0500

    more

--- minikube/java-ads-demo/update.sh
+++ minikube/java-ads-demo/update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "🔄 Updating ads-java-demo deployment..."
+echo "🔄 Updating java-ads-demo deployment..."
 
 # Upgrade the Helm release
 helm upgrade ads-java ./ads-java-chart --namespace demo
 
-echo "✅ ads-java-demo updated successfully."
+echo "✅ java-ads-demo updated successfully."
