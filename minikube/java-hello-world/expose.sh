c63137facb16bb71d88bf90a5f8ead800aa31880
rwirba <rwirba@protonmail.com>
2025-10-23 08:58:46 -0500

    modified:   minikube/expose.sh

--- minikube/java-hello-world/expose.sh
+++ minikube/java-hello-world/expose.sh
@@ -3,7 +3,7 @@ set -e
 
 MINIKUBE_IP=$(minikube ip)
 
-echo "⏳ Waiting for ingress-nginx-controller service..."
+echo "Waiting for ingress-nginx-controller service..."
 until kubectl get svc ingress-nginx-controller -n ingress-nginx &> /dev/null; do sleep 2; done
 
 NODEPORT=$(kubectl get svc ingress-nginx-controller -n ingress-nginx -o jsonpath='{.spec.ports[?(@.port==80)].nodePort}')
@@ -16,4 +16,4 @@ if ! sudo iptables -t nat -C POSTROUTING -p tcp -d ${MINIKUBE_IP} --dport ${NODE
   sudo iptables -t nat -A POSTROUTING -p tcp -d ${MINIKUBE_IP} --dport ${NODEPORT} -j MASQUERADE
 fi
 
-echo "🌐 Access ads app via: http://ads.$(curl -s http://checkip.amazonaws.com).nip.io/"
+echo "Access ads app via: http://ads.$(curl -s http://checkip.amazonaws.com).nip.io/"
