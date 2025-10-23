ad5b331164fe8de990e91df2a39a21fedb9bee70
rwirba <rwirba@protonmail.com>
2025-10-23 10:09:56 -0500

    change ip table

--- minikube/expose.sh
+++ minikube/expose.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
-set -e
 
+NODEPORT=30393
 MINIKUBE_IP=$(minikube ip)
 
 echo "Waiting for ingress-nginx-controller service..."
-until kubectl get svc ingress-nginx-controller -n ingress-nginx &> /dev/null; do sleep 2; done
 
-NODEPORT=$(kubectl get svc ingress-nginx-controller -n ingress-nginx -o jsonpath='{.spec.ports[?(@.port==80)].nodePort}')
+# Flush old rules (optional but clean)
+sudo iptables -t nat -D PREROUTING -p tcp --dport 80 -j DNAT --to-destination $MINIKUBE_IP:$NODEPORT 2>/dev/null
+sudo iptables -t nat -D POSTROUTING -p tcp -d $MINIKUBE_IP --dport $NODEPORT -j MASQUERADE 2>/dev/null
 
-if ! sudo iptables -t nat -C PREROUTING -p tcp --dport 80 -j DNAT --to-destination ${MINIKUBE_IP}:${NODEPORT} 2>/dev/null; then
-  echo "Adding PREROUTING rule..."
-  sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination ${MINIKUBE_IP}:${NODEPORT}
-else
-  echo "PREROUTING rule already exists."
-fi
-
-if ! sudo iptables -t nat -C POSTROUTING -p tcp -d ${MINIKUBE_IP} --dport ${NODEPORT} -j MASQUERADE 2>/dev/null; then
-  echo "Adding POSTROUTING rule..."
-  sudo iptables -t nat -A POSTROUTING -p tcp -d ${MINIKUBE_IP} --dport ${NODEPORT} -j MASQUERADE
-else
-  echo "POSTROUTING rule already exists."
-fi
+# Apply new rules
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $MINIKUBE_IP:$NODEPORT
+sudo iptables -t nat -A POSTROUTING -p tcp -d $MINIKUBE_IP --dport $NODEPORT -j MASQUERADE
 
 echo "Traffic to port 80 is now routed to Ingress NodePort ${NODEPORT} on Minikube IP ${MINIKUBE_IP}"
 echo "Access your apps via:"
