c3c868d36aa1cc694e7838c43763559bf66cd2a9
rwirba <rwirba@protonmail.com>
2025-10-25 00:25:49 -0500

    modified:   minikube/java-hello-world/java-hello-world-chart/templates/pvc.yaml

--- minikube/java-hello-world/deploy.sh
+++ minikube/java-hello-world/deploy.sh
@@ -7,6 +7,6 @@ echo "Deploying Java Hello World app to Minikube..."
 kubectl get ns demo &> /dev/null || kubectl create ns demo
 
 # Deploy Helm chart using values.yaml + dynamic ingress host
-helm upgrade --install hello-java . \
+helm upgrade --install hello-java ./java-hello-world-chart \
   --namespace demo \
   --wait
