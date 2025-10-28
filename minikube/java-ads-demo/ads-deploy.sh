78174608d5e1a20e8151f6811271a79428cae88c
rwirba <rwirba@protonmail.com>
2025-10-28 12:27:15 -0500

    changes

--- minikube/java-ads-demo/ads-deploy.sh
+++ minikube/java-ads-demo/ads-deploy.sh
@@ -5,6 +5,6 @@ echo "Deploying Java Ads Demo app to Minikube..."
 
 kubectl get ns demo &> /dev/null || kubectl create ns demo
 
-helm upgrade --install ads-demo . \
+helm upgrade --install ads-demo ./java-ads-chart \
   --namespace demo \
   --wait
