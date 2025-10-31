3434957742b89f8c65cc86323e7b434d69b99195
rwirba <rwirba@protonmail.com>
2025-10-31 10:00:49 -0500

    modified:   minikube/sap-hana/ansible/roles/sap_hxe/vars/main.yaml

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -14,28 +14,28 @@ data "aws_security_group" "ryan_sg" {
   name = "ryan-dev-sg"
 }
 
-resource "aws_instance" "rhel_demo1" {
-  ami                    = "ami-0dfc569a8686b9320"  # RHEL 9 AMI
-  instance_type          = "t2.medium"
-  key_name               = "ryan-key"
-  iam_instance_profile   = "ryan_dev_lab_instance_role"
-  vpc_security_group_ids = [data.aws_security_group.ryan_sg.id]
+# resource "aws_instance" "rhel_demo1" {
+#   ami                    = "ami-0dfc569a8686b9320"  # RHEL 9 AMI
+#   instance_type          = "t2.medium"
+#   key_name               = "ryan-key"
+#   iam_instance_profile   = "ryan_dev_lab_instance_role"
+#   vpc_security_group_ids = [data.aws_security_group.ryan_sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              dnf -y update
-              dnf -y install git python3-pip
-              pip3 install ansible
-              echo 'export PATH=$PATH:/usr/local/bin' >> /home/ec2-user/.bashrc
-              chown ec2-user:ec2-user /home/ec2-user/.bashrc
-              /usr/local/bin/ansible-galaxy collection install community.general
-              /usr/local/bin/ansible-galaxy collection install ansible.posix
-              EOF
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               dnf -y update
+#               dnf -y install git python3-pip
+#               pip3 install ansible
+#               echo 'export PATH=$PATH:/usr/local/bin' >> /home/ec2-user/.bashrc
+#               chown ec2-user:ec2-user /home/ec2-user/.bashrc
+#               /usr/local/bin/ansible-galaxy collection install community.general
+#               /usr/local/bin/ansible-galaxy collection install ansible.posix
+#               EOF
 
-  tags = {
-    Name = "RHEL9-Demo1"
-  }
-}
+#   tags = {
+#     Name = "RHEL9-Demo1"
+#   }
+# }
 
 # resource "aws_instance" "rhel_demo2" {
 #   ami                    = "ami-0dfc569a8686b9320"
@@ -55,19 +55,35 @@ resource "aws_instance" "rhel_demo1" {
 #   }
 # }
 #
+###############################################
+# SAP HANA Express RHEL9 EC2 Instance (Podman)
+###############################################
 resource "aws_instance" "rhel_demo1" {
-  ami                    = "ami-0dfc569a8686b9320"  # RHEL 9 AMI
-  instance_type          = "t2.medium"
+  ami                    = "ami-0dfc569a8686b9320"  # ✅ Official RHEL 9 AMI (update if region differs)
+  instance_type          = "t3.xlarge"              # ✅ 4 vCPUs, 16 GB RAM (SAP HANA Express minimum)
   key_name               = "ryan-key"
   iam_instance_profile   = "ryan_dev_lab_instance_role"
   vpc_security_group_ids = [data.aws_security_group.ryan_sg.id]
 
+  # Root volume: use gp3 for better performance and throughput
+  root_block_device {
+    volume_size = 80        # ✅ at least 80 GB disk for HANA base image + runtime
+    volume_type = "gp3"
+    delete_on_termination = true
+  }
+
+  ###############################################
+  # Cloud-init: base system + runtime + ansible
+  ###############################################
   user_data = <<-EOF
               #!/bin/bash
               set -eux
+
+              # --- System updates & base packages ---
               dnf -y update
               dnf -y install git python3-pip podman podman-docker buildah skopeo runc
 
+              # --- Configure Podman to always use runc ---
               mkdir -p /etc/containers
               cat <<EOC > /etc/containers/containers.conf
               [engine]
@@ -75,6 +91,7 @@ resource "aws_instance" "rhel_demo1" {
               default_runtime = "runc"
               EOC
 
+              # Rootless config for ec2-user
               sudo -u ec2-user mkdir -p /home/ec2-user/.config/containers
               cat <<EOC > /home/ec2-user/.config/containers/containers.conf
               [engine]
@@ -82,19 +99,27 @@ resource "aws_instance" "rhel_demo1" {
               default_runtime = "runc"
               EOC
               chown -R ec2-user:ec2-user /home/ec2-user/.config
-              podman system migrate || true
-              sudo -u ec2-user podman system migrate || true
-              dnf -y install python3-pip
+
+              # --- Install and prepare Ansible ---
               pip3 install --upgrade pip
               pip3 install ansible
               /usr/local/bin/ansible-galaxy collection install community.general
               /usr/local/bin/ansible-galaxy collection install ansible.posix
+
+              # --- Set environment for ec2-user ---
               echo 'export PATH=$PATH:/usr/local/bin' >> /home/ec2-user/.bashrc
               chown ec2-user:ec2-user /home/ec2-user/.bashrc
+
+              # --- Verify runtime for debugging ---
               podman info | grep -A2 "ociRuntime" >> /var/log/podman-runtime.log 2>&1 || true
               EOF
 
+  ###############################################
+  # Metadata / Tagging
+  ###############################################
   tags = {
-    Name = "RHEL9-Demo1"
+    Name = "RHEL9-HANAExpress-Demo1"
+    Project = "SAP-HANA-Express"
+    Owner   = "ryandevlab"
   }
 }
