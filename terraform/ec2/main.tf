21b1d200fe1eedcb896830b20e2c85e7b2072336
rwirba <rwirba@protonmail.com>
2025-10-31 06:06:25 -0500

    modified:   terraform/ec2/main.tf

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -32,20 +32,20 @@ resource "aws_instance" "rhel_demo1" {
   }
 }
 
-resource "aws_instance" "rhel_demo2" {
-  ami                    = "ami-0dfc569a8686b9320"
-  instance_type          = "t2.medium"
-  key_name               = "ryan-key"
-  iam_instance_profile   = "ryan_dev_lab_instance_role"
-  vpc_security_group_ids = [data.aws_security_group.ryan_sg.id]
+# resource "aws_instance" "rhel_demo2" {
+#   ami                    = "ami-0dfc569a8686b9320"
+#   instance_type          = "t2.medium"
+#   key_name               = "ryan-key"
+#   iam_instance_profile   = "ryan_dev_lab_instance_role"
+#   vpc_security_group_ids = [data.aws_security_group.ryan_sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              dnf -y update
-              dnf -y install git
-              EOF
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               dnf -y update
+#               dnf -y install git
+#               EOF
 
-  tags = {
-    Name = "RHEL9-Demo2"
-  }
-}
+#   tags = {
+#     Name = "RHEL9-Demo2"
+#   }
+# }
