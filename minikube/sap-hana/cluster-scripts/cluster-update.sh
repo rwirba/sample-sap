3d0e7acae1da66bd1b9d926bdb8c9d9a88b01b24
rwirba <rwirba@protonmail.com>
2025-11-05 21:48:12 -0600

    modified:   minikube/sap-hana/cluster-scripts/cluster-update.sh

--- minikube/sap-hana/cluster-scripts/cluster-update.sh
+++ minikube/sap-hana/cluster-scripts/cluster-update.sh
@@ -2,7 +2,7 @@
 set -e
 
 IMAGE="ryandevlab/sap-hana:1.0.0"
-CHART_PATH="./sap-hana-chart"
+CHART_PATH="../sap-hana-chart"
 NAMESPACE="demo"
 
 echo "🔄 Updating SAP HANA Express image and redeploying..."
